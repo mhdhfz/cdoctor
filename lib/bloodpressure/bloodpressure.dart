21f084ed01853aa999ce40598d2d234a920c6798
mhdhfz <mhd.hfzz@gmail.com>
2021-11-26 23:14:57 +0800

    add screen needed

---
+++ lib/bloodpressure/bloodpressure.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BloodPressure extends StatelessWidget {
+  const BloodPressure({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
