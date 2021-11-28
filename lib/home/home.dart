8ad59f3a6bdfbfdde092204feedfbbf5c64fa8b6
mhdhfz <mhd.hfzz@gmail.com>
2021-11-28 11:16:20 +0800

    add auth services

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:cdoctor/login/login.dart';
+import 'package:cdoctor/appointment/appointment.dart';
+import 'package:cdoctor/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            child: Text('appointment'),
-            onPressed: () => Navigator.pushNamed(context, '/appointment'),
-          ) ,
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('error'),
+          );
+        } else if (snapshot.hasData) {
+          return const AppointmentScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
