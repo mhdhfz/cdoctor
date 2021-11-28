225043f6bcd6a4ce6f7920597eafd0887bba7122
mhdhfz <mhd.hfzz@gmail.com>
2021-11-28 09:47:15 +0800

    add routes

---
+++ lib/routes.dart
@@ -0,0 +1,15 @@
+import 'dart:js';
+
+import 'package:cdoctor/about/about.dart';
+import 'package:cdoctor/profile/profile.dart';
+import 'package:cdoctor/appointment/appointment.dart';
+import 'package:cdoctor/bloodpressure/bloodpressure.dart';
+import 'package:cdoctor/home/home.dart';
+
+var appRoutes = {
+  '/': (context) => const HomeScreen(),
+  '/about': (context) => const AboutScreen(),
+  '/profile': (context) => const ProfileScreen(),
+  '/appointment': (context) => const AppointmentScreen(),
+  '/bloodpressure': (context) => const BloodPressure(),
+};
